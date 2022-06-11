6aee6dea02d9be413ec2c8ff688043da41f3ec37
maureenndiema <ndiemam@gmail.com>
2022-06-11 15:10:15 +0300

    create user, p.pic,name bio location models

--- awwardsapp/models.py
+++ awwardsapp/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
 
 class Profile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+    prof_pic=models.ImageField(upload_to='pictures/',default='default.png')
+    name=models.CharField(max_length=50)
+    bio=models.TextField()
+    location=models.CharField(max_length=100)
+    account_url=models.URLField()
+
