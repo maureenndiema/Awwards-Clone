fcd31754a79625eb27d75f2363b2d8ff49b3a642
maureenndiema <ndiemam@gmail.com>
2022-06-12 19:32:37 +0300

    creating class signuoForm

--- awwardsapp/forms.py
+++ awwardsapp/forms.py
@@ -2,3 +2,10 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import *
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1','password2')
