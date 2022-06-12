bdd14ad4b621df759d9c23019df2e89fd75ed96d
maureenndiema <ndiemam@gmail.com>
2022-06-12 19:29:23 +0300

    path for urls.py

---
+++ awwardsapp/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path,re_path
+from django.conf import settings
+from . import views
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('signup/', views.signup_view, name='signup'),
+    path('upload/project/', views.upload_project, name='add_project'),
+    path('',views.welcome,name='welcome'),
+    path('review/<project_title>', views.rate_project, name='rate'),
+    re_path(r'^search/', views.search_project,name='search_results'),
+    path('profile/<username>/', views.profile, name='profile'),
+    path('user_profile/<username>/', views.user_profile, name='user_profile'),
+
+
+
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
